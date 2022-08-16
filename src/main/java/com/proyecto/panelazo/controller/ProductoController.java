4d60cc6f85e2173ca0ed6f3654ea5eb01354c250
Juan Bayona <juanjosbase@gmail.com>
2022-08-16 09:29:04 -0500

    Funcionalidad de guardar producto

--- src/main/java/com/proyecto/panelazo/controller/ProductoController.java
+++ src/main/java/com/proyecto/panelazo/controller/ProductoController.java
@@ -1,18 +1,24 @@
 package com.proyecto.panelazo.controller;
 
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.panelazo.model.Producto;
+import com.proyecto.panelazo.model.Usuario;
+import com.proyecto.panelazo.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
 	
 	@GetMapping("")
 	public String show() {
@@ -27,6 +33,11 @@ public class ProductoController {
 	@PostMapping("/save")
 	public String save(Producto producto) {
 		LOGGER.info("Este es el objeto producto {}",producto);
+		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
+		
+		producto.setUsuario(u);
+		
+		productoService.save(producto);
 		return "redirect:/productos";
 	}
 	
