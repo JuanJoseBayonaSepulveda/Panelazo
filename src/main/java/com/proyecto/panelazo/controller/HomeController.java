d371fe4b025c33dd22179d690dac29d0205df9e1
Juan Bayona <juanjosbase@gmail.com>
2022-09-09 23:38:16 -0500

    mostrar la vista del producto al usuario

--- src/main/java/com/proyecto/panelazo/controller/HomeController.java
+++ src/main/java/com/proyecto/panelazo/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.proyecto.panelazo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.panelazo.service.ProductoService;
@@ -11,6 +14,9 @@ import com.proyecto.panelazo.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
+	
 	
 	@Autowired
 	private ProductoService productoService;
@@ -22,6 +28,12 @@ public class HomeController {
 		
 		return "usuario/home";
 		
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome( @PathVariable Integer id) {
+		log.info("Id producto enviado com oparámetro {}",id);
+		return "usuario/productohome";
 	}
 
 }
