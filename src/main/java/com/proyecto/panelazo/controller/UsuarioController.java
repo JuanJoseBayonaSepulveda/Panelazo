4041877bab04fc7dab4792873c45113aa448a08b
Juan Bayona <juanjosbase@gmail.com>
2022-09-10 12:23:32 -0500

    obtener usuario login y session

--- src/main/java/com/proyecto/panelazo/controller/UsuarioController.java
+++ src/main/java/com/proyecto/panelazo/controller/UsuarioController.java
@@ -1,5 +1,9 @@
 package com.proyecto.panelazo.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +37,34 @@ public class UsuarioController {
 		usuarioService.save(usuario);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "usuario/login";
+	}
+	
+	@PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		logger.info("Accesos : {}",usuario);
+		
+		Optional<Usuario> user= usuarioService.findByEmail(usuario.getEmail());
+		//logger.info("Usuario de DB: {}", user.get());
+		
+		if(user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			if(user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/administrador";
+			}else {
+				return "redirect:/";
+			}
+		}else {
+			logger.info("Usuario no existe");
+		}
+		
+		return "redirect:/";
+		
 	}
 
 }
