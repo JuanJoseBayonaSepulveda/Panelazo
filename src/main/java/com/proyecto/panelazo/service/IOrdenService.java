c87ad39bc2a3103a05fdf4d35a4d4221fc246f64
Juan Bayona <juanjosbase@gmail.com>
2022-09-10 10:35:13 -0500

    Metodo para generar el número de la orden

--- src/main/java/com/proyecto/panelazo/service/IOrdenService.java
+++ src/main/java/com/proyecto/panelazo/service/IOrdenService.java
@@ -1,8 +1,12 @@
 package com.proyecto.panelazo.service;
 
+import java.util.List;
+
 import com.proyecto.panelazo.model.Orden;
 
 public interface IOrdenService {
+	
+	List<Orden> findAll();
 	Orden save(Orden orden);
 
 }
