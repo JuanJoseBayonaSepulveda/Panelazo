a518897d164f2029194bd3de17d9734229fc39d9
Juan Bayona <juanjosbase@gmail.com>
2022-09-10 18:40:15 -0500

    Ver detalle de compra al user

--- src/main/java/com/proyecto/panelazo/service/IOrdenService.java
+++ src/main/java/com/proyecto/panelazo/service/IOrdenService.java
@@ -1,6 +1,7 @@
 package com.proyecto.panelazo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.proyecto.panelazo.model.Orden;
 import com.proyecto.panelazo.model.Usuario;
@@ -8,6 +9,7 @@ import com.proyecto.panelazo.model.Usuario;
 public interface IOrdenService {
 	
 	List<Orden> findAll();
+	Optional<Orden> findById(Integer id);
 	Orden save(Orden orden);
 	String generarNumeroOrden();
 	List<Orden> findByUsuario (Usuario usuario);
