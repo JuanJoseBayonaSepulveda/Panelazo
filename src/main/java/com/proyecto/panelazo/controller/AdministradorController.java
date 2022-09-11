b34bd5ce4cd05a2c5357041496c5137a4c071382
Juan Bayona <juanjosbase@gmail.com>
2022-09-10 19:11:21 -0500

    funcionalidad para ver usuarios admin

--- src/main/java/com/proyecto/panelazo/controller/AdministradorController.java
+++ src/main/java/com/proyecto/panelazo/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.panelazo.model.Producto;
+import com.proyecto.panelazo.service.IUsuarioService;
 import com.proyecto.panelazo.service.ProductoService;
 
 @Controller
@@ -18,6 +19,9 @@ public class AdministradorController {
 	@Autowired
 	private ProductoService productoService;
 	
+	@Autowired
+	private IUsuarioService usuarioService;
+	
 	@GetMapping("")
 	public String home(Model model) {
 		
@@ -25,6 +29,12 @@ public class AdministradorController {
 		model.addAttribute("productos",productos);
 		
 		return "administrador/home";
+	}
+	
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		model.addAttribute("usuarios", usuarioService.findAll());
+		return "administrador/usuarios";
 	}
 
 }
